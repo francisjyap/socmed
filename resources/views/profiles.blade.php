9b1b1e595fbfb27017cb6503457021b8acc65020
Francis Yap <francisj.yap@gmail.com>
2018-03-23 18:25:56 +0800

    minor UI changes

--- resources/views/profiles.blade.php
+++ resources/views/profiles.blade.php
@@ -6,9 +6,10 @@
 
 	<div class="row">
         <div class="col-md-8 offset-2">
-        	<h3>Profiles</h3>
-        	<a href="#" class="btn btn-success" style="float: right;">Add New Profile</a>
-    		<table class="table table-striped" id="table"></table>
+        	<h3 style="margin-top: 5%;">Profiles</h3>
+        	<a href="#" class="btn btn-success" style="float: right;  margin-bottom: 5%;">Add New Profile</a>
+    		
+    		<table id="table"></table>
         </div>
 		
 	</div>
